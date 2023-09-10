c6d36d166ad177b56b2f6e02b74947e1c3337d98
RajuRanjanCse <144608595+RajuRanjanCse@users.noreply.github.com>
2023-09-10 15:00:06 +0000

    data type and variabe

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,8 @@
 console.log("Raju Ranjan");
+
+let userId="rajuranjan";
+var password=123456; //var we use for global variable , avoid to use it, bacause if you change in 1 place 
+//it will change variable at multiples scope
+const pincode=124507;
+
+console.table([userId,password,pincode]);
